9add116783584e3cfe7209ccabf09c8adc7af50c
Ayofe Faheez <ayofefaheez@gmail.com>
2023-08-24 15:30:18 -0800

    home page

--- src/Home.js
+++ src/Home.js
@@ -10,16 +10,52 @@ function Home() {
         />
 
         <div className="home__row">
-            <Product />
-            <Product />
+            <Product
+            id = "6753421" 
+            title="Four Candies Cute Mechanical Pencil Set, 6PCS Pastel Mechanical Pencils 0.5mm &amp; 0.7mm with 360PCS HB Pen..."
+            price={11.59}
+            image= {require("./images/product-one.webp")}
+            rating={5}
+            />
+            <Product 
+            id = "7689654" 
+            title="Apple iPad (9th Generation): with A13 Bionic chip, 10.2-inch Retina Display, 64GB, Wi-Fi, 12MP front/8MP Back Camera..."
+            price={102.67}
+            image= {require("./images/product-two.jpg")}
+            rating={5}
+            />
         </div>
         <div className="home__row">
-            {/* product*/}
-            {/* product*/}
-            {/* product*/}
+            <Product 
+            id = "7864312"
+            title="Emica Home Glass Storage Jars with Bamboo Airtight Lids..."
+            price={24.99}
+            image= {require("./images/product-three.webp")}
+            rating={5}
+            />
+            <Product 
+            id = "4321098"
+            title="Speed Stick Men's Deodorant, Regular, 3 Ounce, 4 Pack"
+            price={9.70}
+            image= {require("./images/product-four.webp")}
+            rating={5}
+            />
+            <Product 
+            id = "2314567"
+            title="Marcy Dual Action Cross Training Recumbent..."
+            price={11.20}
+            image= {require("./images/product-five.webp")}
+            rating={5}
+            />
         </div>
         <div className="home__row">
-            {/* product*/}
+          <Product 
+          id = "16543209"
+          title="Sceptre Curved 24.5-inch Gaming Monitor up to 240Hz 1080p R1500 1ms DisplayPort x2 HDMI x2 Blue Light..."
+          price={149.97}
+          image= {require("./images/product-six.jpg")}
+          rating={5}
+          />
         </div>
       </div>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,21 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({title, image, price, rating}) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐</p>
+          {Array(rating).fill().map((_, i) =>  (
+            <p>⭐</p> ))}
         </div>
       </div>
-      <img src={require("./images/product-one.webp")} />
+      <img src={image} />
       <button>Add to Cart</button>
     </div>
   );
