4c76cb3fc25767098e877bdec87f499a4fbad675
Ayofe Faheez <ayofefaheez@gmail.com>
2023-08-27 16:19:36 -0800

    Updates

---
+++ precache-manifest.d51428f09c7234f61efd9a1e0f8e0625.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ddc20b608f9927976ef",
+    "url": "/Amazon-clone/static/css/main.8e0bd683.chunk.css"
+  },
+  {
+    "revision": "8ddc20b608f9927976ef",
+    "url": "/Amazon-clone/static/js/main.1b4d54fc.chunk.js"
+  },
+  {
+    "revision": "50baf10a4bef3e2bb22a",
+    "url": "/Amazon-clone/static/js/runtime~main.532781ea.js"
+  },
+  {
+    "revision": "4b0d92b67623b43cbbc2",
+    "url": "/Amazon-clone/static/js/2.0bdb5de9.chunk.js"
+  },
+  {
+    "revision": "2d5464cc6be78253789c",
+    "url": "/Amazon-clone/static/js/3.7f06be4c.chunk.js"
+  },
+  {
+    "revision": "1a7d775d8d218f2393fd8848a386ef6e",
+    "url": "/Amazon-clone/static/media/amazon-logo.1a7d775d.png"
+  },
+  {
+    "revision": "aced0300b9b0e29ab903610ebf89298b",
+    "url": "/Amazon-clone/static/media/bg-img.aced0300.jpg"
+  },
+  {
+    "revision": "f4fa4681b80e403b2ce432771a474554",
+    "url": "/Amazon-clone/static/media/product-one.f4fa4681.webp"
+  },
+  {
+    "revision": "fae96d81658b92a9f04ccf4505c702b9",
+    "url": "/Amazon-clone/static/media/product-two.fae96d81.jpg"
+  },
+  {
+    "revision": "45d10274cb7e9b15bc828cbd255e3326",
+    "url": "/Amazon-clone/static/media/product-three.45d10274.webp"
+  },
+  {
+    "revision": "be7cba6614e8916d6ee80268b1c58821",
+    "url": "/Amazon-clone/static/media/product-four.be7cba66.webp"
+  },
+  {
+    "revision": "4403e4bf416aa4822e121534e81014ac",
+    "url": "/Amazon-clone/static/media/product-five.4403e4bf.webp"
+  },
+  {
+    "revision": "129e6e9227b5de4c13a3eb939a770dca",
+    "url": "/Amazon-clone/static/media/product-six.129e6e92.jpg"
+  },
+  {
+    "revision": "282b54b6c3145755d492c2e6f73725c0",
+    "url": "/Amazon-clone/static/media/ad.282b54b6.jpeg"
+  },
+  {
+    "revision": "83c70fb15f442f0c85ecda88f01736ae",
+    "url": "/Amazon-clone/index.html"
+  }
+];
